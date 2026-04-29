bf0fec1cbffd7ce8a8ebe89aac1d00104535228b
twelve-jasmine <3997497688@qq.com>
2026-04-29 20:34:59 +0800

    Add files via upload

---
+++ practice_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	int t;
+	cin >> a >> b >> c;
+	a > b ? t = a, a = b, b = t : 0;
+	a > c ? t = a, a = c, c = t : 0;
+	b > c ? t = b, b = c, c = t : 0;
+	cout << a << "<" << b << "<" << c << endl;
+
+	return 0;
+}
